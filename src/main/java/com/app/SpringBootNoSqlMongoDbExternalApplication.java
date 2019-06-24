2b691ca8ea4d23f348b9dc5add53965251514062
ajitsase <34818235+ajitsase@users.noreply.github.com>
2019-06-24 10:16:12 +0530

    Application to No SQL database MongoDb by using External installed database.

---
+++ src/main/java/com/app/SpringBootNoSqlMongoDbExternalApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNoSqlMongoDbExternalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNoSqlMongoDbExternalApplication.class, args);
+	}
+
+}
